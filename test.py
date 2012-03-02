38aa32f5136e788ac33d1b401410518ed7ea2111
Greg Belanger <agilekalaf@gmail.comgit config --global user.email agilekalaf@gmail.comgit config --global user.email agilekalaf@gmail.com>
2012-03-01 23:31:27 -0700

    new files

--- test.py
+++ test.py
@@ -3,6 +3,11 @@ Created on Mar 1, 2012
 
 @author: gregbelanger
 '''
+'''
+Created on Mar 1, 2012
+
+@author: gregbelanger
+'''
 def sayHi():
     return 'Hi'
 
---
+++ testgit.py
@@ -0,0 +1,6 @@
+'''
+Created on Mar 1, 2012
+
+@author: gregbelanger
+'''
+print('hello')
