fe2730c0fc59314ae9eebc500c83f64c7dd013d5
Greg Belanger <agilekalaf@gmail.comgit config --global user.email agilekalaf@gmail.comgit config --global user.email agilekalaf@gmail.com>
2012-03-07 20:52:04 -0700

    corrected HW --I suck

--- FindLast.py
+++ FindLast.py
@@ -4,19 +4,20 @@ Created on Mar 3, 2012
 @author: gregbelanger
 '''
 def find_last(target, search):
-    if target.find(search) == -1:
-        print(-1)
+    start = target.find(search)
+    if start == -1:
+        return -1
     else:
-        start = target.find(search)
-        while start != -1:
-            #print(start)
-            start = target.find(search,start+1)
-        print(target.find(search,start))
-        return
-        
+        while True:
+            if start != -1:
+                pos = start
+                start = target.find(search,start+1)
+            else:
+                return pos
+            
         
     
         
 
 
-find_last('asdasdasdasda','a')
+print(find_last('aaaa','b'))
