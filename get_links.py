6a4311821fcd120be42b5d45c6476dda1dbeac00
Greg Belanger <agilekalaf@gmail.comgit config --global user.email agilekalaf@gmail.comgit config --global user.email agilekalaf@gmail.com>
2012-03-10 19:06:16 -0700

    web crawler...check

--- get_links.py
+++ get_links.py
@@ -18,12 +18,36 @@ def print_all_links(page):
             print("no urls")
             break
         
+def get_all_links(page):
+    link_list = []
+    while True:
+        url, endpos = get_next_target(page)
+        if url:
+            link_list.append(url)
+            page = page[endpos:]
+        else:
+            #break
+            print("no urls")
+            break
+    return link_list
+
+def crawl_web(seed):
+    tocrawl = [seed]
+    crawled = []
+    while tocrawl:
+        page = tocrawl.pop()
+        if page not in crawled:
+            #crawl
+            tocrawl.append(get_all_links(page))
+            crawled.append(page)
+    return crawled
 
 
 #url, end_pos = get_next_target('this is a <a href="http://udacity.com">link!</a>')
 #url, end_pos = get_next_target('this" is a"')
 #print(url)
-targetString = 'Hello World'
-#targetString = 'this is a <a href="http://udacity.com">link!</a>. if you like this site you will also like <a href="http://google.com">Google</a>.'
+#targetString = 'Hello World'
+targetString = 'this is a <a href="http://udacity.com">link!</a>. if you like this site you will also like <a href="http://google.com">Google</a>.'
 #print_all_links('this is a <a href="http://udacity.com">link!</a>. if you like this site you will also like <a href="http://google.com">Google</a>.')
 print_all_links(targetString)
+print(get_all_links(targetString))
